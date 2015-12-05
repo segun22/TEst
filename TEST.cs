c5b7ab9db2694e945246520014167722a6431a67
TaggGuy <exitfill1234@naver.com>
2015-12-05 13:20:29 +0900

    adsf
    asdfasdf

---
+++ TEST.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TEST : MonoBehaviour {
+
+	public GameObject hi;
+
+	// Use this for initialization
+	void Start () {
+		hi = GameObject.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hi.GetComponent<Transform> ().position = new Vector3 (10, 10, 10);s
+	}
+}
